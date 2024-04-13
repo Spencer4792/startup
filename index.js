712e7c31e3153b2ab36716294eda7e16e3fcaa31
Spencer Hales <spencerhales4792@gmail.com>
2024-04-12 21:59:55 -0600

    Added AuthContext.js and modified index.js to add user authentication.

--- index.js
+++ index.js
@@ -16,3 +16,20 @@ axios.interceptors.request.use(function (config) {
     config.headers.Authorization = token ? `Bearer ${token}` : '';
     return config;
 });
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
