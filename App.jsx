62808a7fdcdf27e72916f22390806d40a0a8a42a
Spencer Hales <spencerhales4792@gmail.com>
2024-04-12 18:45:47 -0600

    Added index.js, added hook to index.html, and worked on framework in App.jsx.

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
 import './styles.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import Home from './Home';
+import About from '.';
+import Contact from '';
 import Signup from '';
 // Import other page components as needed
 
